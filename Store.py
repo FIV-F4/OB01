03969749b6414eb15d696f6ae838c09a5124f0fc
FIV-F4 <9856142@mail.ru>
2024-05-05 21:38:48 +0300

    update class Store

--- Store.py
+++ Store.py
@@ -4,13 +4,26 @@ class Store:
         self.location = location
         self.items = {}
     def add_item(self, item, price):
-        pass
+        if item in self.items:
+            print("Товар уже существует")
+        else:
+            self.items[item] = price
+
     def remove_item(self, item):
-        pass
-    def show_price_item(self):
-        pass
-    def update_price_item(self):
-        pass
+        if item in self.items:
+            del self.items[item]
+        else:
+            print("Такого товара нет")
+    def show_price_item(self, item):
+        if item in self.items:
+            return self.items[item]
+        else:
+            print("Такого товара нет")
+    def update_price_item(self, item, new_price):
+        if item in self.items:
+            self.items[item] = new_price
+        else:
+            print("Такого товара нет")
 
 
 Store1 = Store("Shop", "Moscow")
@@ -18,5 +31,18 @@ Store2 = Store("KB", "SPB")
 Store3 = Store("NY", "NY")
 
 Store1.add_item("Гвоздь", 10)
-Store2.add_item("Дубель", 15)
-Store3.add_item("Линейка", 20)
+Store1.add_item("Дубель", 15)
+Store1.add_item("Гвоздь", 10)
+Store1.add_item("Линейка", 20)
+print(Store1.items)
+
+Store1.remove_item("Гвоздь")
+print(Store1.items)
+
+print(Store1.show_price_item("Линейка"))
+
+Store1.update_price_item("Линейка", 25)
+Store1.update_price_item("Арбуз", 25)
+print(Store1.items)
+
+
