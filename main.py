1921b31c749239f67033461869f9d05263442748
FIV-F4 <9856142@mail.ru>
2024-05-05 21:13:41 +0300

    Add classmethod

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 class Task:
+    tasks = []
     def __init__(self, description, due_date, completed=False):
         self.description = description
         self.due_date = due_date
         self.completed = completed
+        Task.tasks.append(self)
 
-    def show_all_tasks(self):
-        pass
-    def add_tasks(self):
-        pass
+    @classmethod
+    def show_all_tasks(cls):
+        if cls.tasks:
+            for task in cls.tasks:
+                if not task.completed:
+                    print(f"Описание: {task.description}, Срок: {task.due_date}, Статус: Не выполнено")
+        else:
+            print("Задач нет.")
+    @classmethod
+    def add_task(cls, description, due_date):
+        return cls(description, due_date)
 
     def mark_completed(self):
         self.completed = True
+
+task1 = Task.add_task("Купить продукты", "2024-05-10")
+task2 = Task.add_task("Посетить врача", "2024-05-15")
+task3 = Task.add_task("Пройти следующий урок в zerocoder", "2024-05-12")
+
+task1.mark_completed()
+
+Task.show_all_tasks()
